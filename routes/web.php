5dd3dc30e3c66ccd97b7b8e2b0094114d841ac6f
kaishi12 <50234328+kaishi12@users.noreply.github.com>
2020-05-22 10:30:33 +0700

    Update Route

--- routes/web.php
+++ routes/web.php
@@ -12,10 +12,41 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+Route::group(['middleware' => 'locale'], function() {
+	Route::get('change-language/{language}', 'Admin\HomeController@ChangeLanguage') ->name('user.change-language');
+});
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('foo', function () {
-    return 'Hello World';
-});
+//User
+Route::get('/Hellowebsite','Admin\HelloController@getHello');
+Route::get('/GetListCategory','Admin\HelloController@getCategory');
+Route::post('/Account/PrepareEdit','Admin\HelloController@PrepareEdit');
+Route::post('/Account/Edit','Admin\HelloController@Edit');
+Route::post('/Account/Delete','Admin\HelloController@Delete');
+Route::post('/Account/Add','Admin\HelloController@AddNew');
+
+//Book
+Route::get('/Book','Hieuadmin\BookController@getview');
+Route::get('/Book/Getlist','Hieuadmin\BookController@getlist');
+Route::get('/Book/GetGenre','Hieuadmin\BookController@getgenrelist');
+Route::post('/Book/PrepareEdit','Hieuadmin\BookController@PrepareEdit');
+Route::post('/Book/Edit','Hieuadmin\BookController@Edit');
+Route::post('/Book/Delete','Hieuadmin\BookController@Delete');
+Route::post('/Book/Add','Hieuadmin\BookController@AddNew');
+
+//Genre
+Route::get('/Genre','Hieuadmin\GenreController@getview');
+Route::get('/Genre/Getlist','Hieuadmin\GenreController@getlist');
+Route::post('/Genre/PrepareEdit','Hieuadmin\GenreController@PrepareEdit');
+Route::post('/Genre/Edit','Hieuadmin\GenreController@Edit');
+Route::post('/Genre/Delete','Hieuadmin\GenreController@Delete');
+Route::post('/Genre/Add','Hieuadmin\GenreController@AddNew');
+
+//Card
+Route::get('/Card','Hieuadmin\CardController@getview');
+Route::get('/Card/Getlist','Hieuadmin\CardController@getlist');
+Route::post('/Card/PrepareEdit','Hieuadmin\CardController@PrepareEdit');
+Route::post('/Card/Edit','Hieuadmin\CardController@Edit');
+Route::post('/Card/Delete','Hieuadmin\CardController@Delete');
+Route::post('/Card/Add','Hieuadmin\CardController@AddNew');
