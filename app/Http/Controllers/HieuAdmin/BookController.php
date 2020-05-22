54e58094905b91ea63fe995ceb4257bad29ce478
kaishi12 <50234328+kaishi12@users.noreply.github.com>
2020-05-22 22:31:34 +0700

    Book - List - Edit - Add - Delete

---
+++ app/Http/Controllers/HieuAdmin/BookController.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace App\Http\Controllers\HieuAdmin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
+use App\Book;
+use App\Genre;
+use App\GOB;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+class BookController extends Controller
+{
+    public function getview()
+    {
+        return view('Hieuadmin.book.book');
+    }
+    public function getlist()
+    {
+        $count1 = 1;
+        $posts = Book::where('active',1)->get();
+        $data = [];
+        foreach ($posts as $post) {
+            $data2 = [];
+            
+            array_push($data2, $count1++, $post->name,$post->author,$post->bookshelves,$post->total,$post->active,$post->id);
+            
+
+            array_push($data,$data2);
+        }
+        echo json_encode($data);
+    }
+    public function getgenrelist()
+    {
+        $posts = Genre::get();
+        echo json_encode($posts);
+    }
+    public function PrepareEdit(Request $request)
+    {
+        $book = Book::where('id', $request->input('id'))->where('active',1)->first();
+        $gob = GOB::where('bookid',$request->input('id'))->where('active',1)->get();
+        $data = [];
+        foreach ($gob as $genre) {
+            array_push($data,$genre->genreid);
+        }
+        $json_data = array(
+            "book"   => $book,  
+            "gob"    => $data,  
+           
+            );
+        echo json_encode($json_data);
+    }
+    public function Edit(Request $request)
+    {
+        $book = Book::where('id', $request->input('id'))->first();
+        $book->name = $request->get('name');
+        $book->bookshelves = $request->get('shelf');
+        $book->author = $request->get('author');
+        $book->total = $request->get('total');
+        $book->description = $request->get('des');
+        $book->save();
+        $gob = GOB::where('bookid',$request->input('id'))->where('active',1)->get();
+        
+        foreach ($gob as $genre) {
+            
+            if(!in_array(strval($genre->genreid),$request->get('genre')))
+            {
+                $genre->active = 0;
+                $genre->save();
+            }
+        }
+        foreach ($request->get('genre') as $genre) {
+            $de = $gob->where('genreid',(int)$genre)->first();
+            if($de == null)
+            {
+                $gob = new GOB([
+                    'bookid' =>$request->input('id'),
+                    'genreid' => (int)$genre,
+                    'active' => 1,
+                ]);
+                $gob->save();
+            }
+        }
+        
+        $mess = true;
+        echo json_encode($mess);
+    }
+    public function Delete(Request $request)
+    {
+        $account = Book::where('id', $request->input('id'))->first();
+        $account->active = 0;
+        $account->save();
+        $mess = true;
+        echo json_encode($mess);
+    }
+    public function AddNew(Request $request)
+    {
+        $account = new Book([
+            'name' => $request->get('name'),
+            'bookshelves' => $request->get('shelf'),
+            'active' => 1,
+            'author' => $request->get('author'),
+            'total' => $request->get('total'),
+            'description' => $request->get('des'),
+        ]);
+        $account->save();
+        $id = $account->id;
+        
+        foreach ($request->get('genre') as $genre) {
+            $gob = new GOB([
+                'bookid' =>$id,
+                'genreid' => (int)$genre,
+                'active' => 1,
+                
+            ]);
+            $gob->save();
+        }
+        $mess = true;
+        echo json_encode($mess);
+    }
+}
---
+++ resources/views/Hieuadmin/book/book.blade.php
@@ -0,0 +1,428 @@
+@extends('admin.master')
+
+@section('content')
+<style>
+.disabled {
+        pointer-events: none;
+        cursor: not-allowed;
+        opacity: 0.5;
+    }
+</style>
+<div class="sidebar-overlay" id="sidebar-overlay"></div>
+<div class="sidebar-mobile-menu-handle" id="sidebar-mobile-menu-handle"></div>
+<div class="mobile-menu-handle"></div>
+<article class="content items-list-page">
+    <div class="title-search-block">
+        <div class="title-block">
+           
+               
+                    <h3  style="font-size: 25px;"> Danh sách tài khoản
+                    </h3>
+                    <button type="button" class="btn ShowPopup btn-primary col-sm-2 offset-sm-10" data-toggle="modal" data-target="#AddModal">Thêm mới</button>
+                    
+        </div>
+        <table class="table table-bordered" id="posts">
+            <thead>
+                <th>{{__('No.')}}</th>
+                <th>Tên</th>
+                <th>tác giả</th>
+                <th>giá sách</th>
+                <th>số lượng</th>
+                <th>trạng thái</th>
+                <th>tùy chọn</th>
+            </thead>
+            <tbody></tbody>
+        </table>
+        <div id="AddModal" class="modal" data-backdrop="true">
+        <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Thêm sách</h5>
+            </div>
+            <div class="modal-body text-center p-lg">
+                <div class="form-group row">
+                    <label class="col-sm-4 form-control-label">
+                        <span>Tên sách</span>
+                    </label>
+                    <div class="col-sm-8">
+                        <input id="name" class="form-control" />
+                    </div>
+
+                </div>
+                <div class="form-group row">
+                    <label class="col-sm-4 form-control-label">
+                        <span>Tác giả</span>
+                    </label>
+                    <div class="col-sm-8">
+                        <input id="author" class="form-control" />
+                    </div>
+
+                </div>
+                <div class="form-group row">
+                    <label class="col-sm-4 form-control-label">
+                        <span>giá sách</span>
+                    </label>
+                    <div class="col-sm-8">
+
+                        <input id="shelf" class="form-control" />
+                    </div>
+
+                </div>
+                <div class="form-group row">
+                    <label class="col-sm-4 form-control-label">
+                        <span>số lượng</span>
+                    </label>
+                    <div class="col-sm-8">
+
+                        <input id="total" type="email" class="form-control" />
+                    </div>
+
+                </div>
+                <div class="form-group row">
+                    <label class="col-sm-4 form-control-label">
+                        <span>Thể loại</span>
+                    </label>
+                    <div class="col-sm-8">
+                    <select id="genre" style="width:100%" class=" js-example-basic-multiple"  multiple="multiple">
+  
+                    </select>
+                    </div>
+
+                </div>
+                <div class="form-group row">
+                    <label class="col-sm-4 form-control-label">
+                        <span>Mô tả</span>
+                    </label>
+                    <div class="col-sm-8">
+
+                        <input id="des" type="email" class="form-control" />
+                    </div>
+
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-danger" data-dismiss="modal">Thoát</button>
+                <button type="button" class="btn btn-primary" id="SaveAdd">Thêm</button>
+            </div>
+        </div><!-- /.modal-content -->
+    </div>
+    
+</div>
+<div id="EditModal" class="modal" data-backdrop="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id='usernametitle'></h5>
+                <input hidden id="id" class="form-control" />
+            </div>
+            <div class="modal-body text-center p-lg">
+                <div class="form-group row">
+                    <label class="col-sm-4 form-control-label">
+                        <span>Tên sách</span>
+                    </label>
+                    <div class="col-sm-8">
+                        <input id="nameedit" class="form-control" />
+                    </div>
+
+                </div>
+                <div class="form-group row">
+                    <label class="col-sm-4 form-control-label">
+                        <span>Tác giả</span>
+                    </label>
+                    <div class="col-sm-8">
+                        <input id="authoredit" class="form-control" />
+                    </div>
+
+                </div>
+                <div class="form-group row">
+                    <label class="col-sm-4 form-control-label">
+                        <span>giá sách</span>
+                    </label>
+                    <div class="col-sm-8">
+
+                        <input id="shelfedit" class="form-control" />
+                    </div>
+
+                </div>
+                <div class="form-group row">
+                    <label class="col-sm-4 form-control-label">
+                        <span>số lượng</span>
+                    </label>
+                    <div class="col-sm-8">
+
+                        <input id="totaledit"  class="form-control" />
+                    </div>
+
+                </div>
+                <div class="form-group row">
+                    <label class="col-sm-4 form-control-label">
+                        <span>Thể loại</span>
+                    </label>
+                    <div class="col-sm-8">
+                    <select id="genreedit" style="width:100%" class=" js-example-basic-multiple"  multiple="multiple">
+  
+                    </select>
+                    </div>
+
+                </div>
+                <div class="form-group row">
+                    <label class="col-sm-4 form-control-label">
+                        <span>Mô tả</span>
+                    </label>
+                    <div class="col-sm-8">
+
+                        <input id="desedit" type="email" class="form-control" />
+                    </div>
+
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-danger" data-dismiss="modal">Thoát</button>
+                <button type="button" class="btn btn-primary" id="SaveEdit">Thêm</button>
+            </div>
+        </div><!-- /.modal-content -->
+    </div>
+{{ csrf_field() }}
+</article>
+<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/js/select2.min.js"></script>
+<script>
+    $('.ShowPopup').on('click', function(event) {
+        var button = $(event.relatedTarget);
+        var recipient = button.data('whatever');
+        var modal = $(this);
+        modal.find('.modal-title').text('New message to ' + recipient);
+        modal.find('.modal-body input').val(recipient);
+    })
+
+    $(document).ready(function() {
+        $('.js-example-basic-multiple').select2();
+        $('#genreedit').select2();
+        LoadCategory();
+        LoadGenre();
+    });
+    function LoadCategory()
+    {
+        $.ajax({
+                type: 'GET',
+                url: '/Book/Getlist',
+                success: function(result) {
+                    initTimeFrameDatatable(JSON.parse(result));
+                   console.log(JSON.parse(result));
+                },
+                error: function(error) {
+                    alert("Có lỗi xảy ra, vui lòng thử lại", 1);
+                }
+            });
+    }
+    function LoadGenre()
+    {
+        $.ajax({
+                type: 'GET',
+                url: '/Book/GetGenre',
+                success: function(result) {
+                   console.log(JSON.parse(result));
+                   $.each(JSON.parse(result), function (arrayID, element) {
+                        $("#genre").append('<option value='+element.id+'>'+element.name+'</option>');
+                        $("#genreedit").append('<option value='+element.id+'>'+element.name+'</option>');
+                    });
+                },
+                error: function(error) {
+                    alert("Có lỗi xảy ra, vui lòng thử lại", 1);
+                }
+            });
+    }
+    function initTimeFrameDatatable(data) {
+            $("#posts").dataTable().fnDestroy();
+            $("#posts").dataTable({
+                stateSave: true,
+                "bFilter": true,
+                "bSort": false,
+                "data": data,
+                "bRetrieve": true,
+                "bScrollCollapse": true,
+                "bProcessing": true,
+                "oLanguage": {
+                    "sSearch": "...",
+                    "sSearchPlaceholder": "User",
+                    "sZeroRecords": "Không có dữ liệu phù hợp",
+                    "sInfo": "Hiển thị từ _START_ đến _END_ trên tổng số _TOTAL_ dòng",
+                    "sEmptyTable": "Không có dữ liệu",
+                    "sInfoFiltered": " - lọc ra từ _MAX_ dòng",
+                    "sLengthMenu": "Hiển thị _MENU_ dòng",
+                    "sProcessing": "Đang xử lý...",
+                    "oPaginate": {
+                        "sNext": "<i class='fa fa-chevron-right'></i>",
+                        "sPrevious": "<i class='fa fa-chevron-left'></i>"
+                    }
+                },
+                "aoColumnDefs": [
+                    {
+                        "aTargets": [0, 1,2,3,4,5],
+                        "sClass": "text-center",
+                        "bSortable": false
+                    },
+                    {
+                        "aTargets": [5],
+                        "mRender": function (data, type, row) {
+                            if(data == 1)
+                            {
+                                return '<span class="btn btn-primary disabled">Đang hoạt động</span>';
+                            }
+                            else
+                            {
+                                return '<span class="btn btn-warning disabled">Ngưng hoạt động</span>'
+                            }
+                            
+                        },
+                    },
+                    {
+                        "aTargets": [6],
+                        "mRender": function (data, type, row) {
+                            var edit =
+                                "<a title='Sửa' class='btn btn-sm btn-primary' onclick='showEditModal(" +
+                                data +
+                                ")'>Sửa</a>";
+                            var del =
+                                "<a title='Xóa' class='btn btn-sm btn-primary' onclick='Delete(" +
+                                data +
+                                ")'>Xóa</a>";
+                            return edit+ " " + del ;
+                        },
+                    }
+                ],
+                "bAutoWidth": false,
+            });
+
+    }
+
+    function showEditModal(id) {
+         $.ajax({
+                type: 'POST',
+                url: '/Book/PrepareEdit',
+             data: {
+                _token: "{{csrf_token()}}",
+                 id: id
+             },
+             success: function (results) {
+                 let result = JSON.parse(results);
+                 console.log(result);
+                 $("#id").val(result.book.id);
+                 $("#nameedit").val(result.book.name);
+                 $("#shelfedit").val(result.book.bookshelves);
+                 $("#authoredit").val(result.book.author);
+                 $("#totaledit").val(result.book.total);
+                 $("#desedit").val(result.book.description);
+                 $("#genreedit").val(result.gob).change();
+                console.log(result.gob);
+                }
+            });
+         $('#EditModal').modal('show');
+
+    }
+    function ResetEditModal() {
+        $("#usernameedit").val("");
+                 $("#pwdedit").val("");
+                 $("#emailedit").val("");
+       
+    }
+    function ResetAddModal() {
+        $("#username").val("");
+                 $("#pwd").val("");
+                 $("#email").val("");
+       
+    }
+    $("#SaveEdit").on("click", function () {
+        
+         if ($('#nameEdit').val() == "") {
+            alert("Vui lòng nhập tên thể loại");
+                return;
+            }
+            
+            $.ajax({
+                type: 'POST',
+                url: '/Book/Edit',
+                data: {
+                    _token: "{{csrf_token()}}",
+                    'id':$("#id").val(),
+                    'name': $("#nameedit").val(),
+                    'author': $("#authoredit").val(),
+                    'total': +$("#totaledit").val(),
+                    'shelf': +$("#shelfedit").val(),
+                    'genre': $("#genreedit").val(),
+                    'des':$("#desedit").val(),
+                },
+                success: function(result) {
+                    if (result == 'true') {
+                        alert("Chỉnh sửa thành công");
+                        $('#EditModal').modal('hide');
+                        ResetEditModal();
+                    } else {
+                        alert("Có lỗi xảy ra, vui lòng thử lại");
+                    }
+                },
+                error: function(error) {
+                    alert("Có lỗi xảy ra, vui lòng thử lại");
+                }
+            });
+    })
+      $("#SaveAdd").on("click", function () {
+         if ($('#name').val() == "") {
+            alert("Vui lòng nhập tên trạng thái");
+                return;
+            }
+           
+            $.ajax({
+                type: 'POST',
+                url: '/Book/Add',
+                data: {
+                    _token: "{{csrf_token()}}",
+                    'name': $("#name").val(),
+                    'author': $("#author").val(),
+                    'total': +$("#total").val(),
+                    'shelf': +$("#shelf").val(),
+                    'genre': $("#genre").val(),
+                    'des':$("#des").val(),
+                },
+                success: function(result) {
+                    if (result.rs == true) {
+                        alert(result.mess);
+                        LoadCategory();
+                        $('#AddModal').modal('hide');
+                        ResetAddModal();
+                    } else {
+                        alert(result.mess);
+                    }
+                },
+                error: function(error) {
+                    ShowAlert("Có lỗi xảy ra, vui lòng thử lại", 1);
+                }
+            });
+      })
+    function Delete(id) {
+
+        var r = confirm("Bạn có muốn khóa tài khoản này?");
+            if (r == true) {
+                    $.ajax({
+                        type: 'POST',
+                        url: '/Book/Delete',
+                        data: {
+                            _token: "{{csrf_token()}}",
+                            'id': id,
+                        },
+                        success: function(data) {
+                            if (data == 'true') {
+                                alert("Khóa thành công");
+                                LoadCategory();
+                            } else {
+                                alert('Có lỗi đã xảy ra, vui lòng liên hệ admin');
+                            }
+                        },
+                        error: function (error) {
+                            alert('Có lỗi đã xảy ra, vui lòng liên hệ admin');
+                        }
+                    });
+}
+    }
+</script>
+
+@endsection
